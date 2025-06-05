eba9c06d087a3a098aaba2e08d5b91aa1ee1fdfc
Meiling Miller <millemei@flip3.engr.oregonstate.edu>
2025-06-05 11:05:19 -0700

    COMPLETED DUMP/BACKTRACE

--- debug_skeleton_code/src/asm/dump_backtrace.s
+++ debug_skeleton_code/src/asm/dump_backtrace.s
@@ -8,38 +8,24 @@ unknown_file: .asciz "??"
 
 .section .text
 dump_backtrace:
-    # Function prologue
+    # stores base pointer
     pushq %rbp
     movq %rsp, %rbp
-   
-    # Save callee-saved registers
-    pushq %rbx
-    pushq %r12
-    pushq %r13
-    pushq %r14
-    pushq %r15
-   
+    
+    
     # Allocate 64 bytes on stack for Dl_info + alignment
     subq $64, %rsp
     
     # Initialize depth counter
     xorq %r12, %r12          # r12 = depth = 0
-   
+    
     # Start with current frame pointer (don't skip caller)
     movq %rbp, %r13          # r13 = current frame pointer
 
 backtrace_loop:    
-    # Checks the maximum depth limit (in order to prevent runaway)
-    cmpq $50, %r12
-    jge backtrace_done
-    
     # Check if frame pointer is valid
     testq %r13, %r13
     jz backtrace_done
-    
-    # Another check to see if register is equal to or above 0x10000
-    cmpq $0x1000, %r13       # this is the minimum reasonable address
-    jb backtrace_done
     
     # Get return address from current frame
     movq 8(%r13), %r14       # r14 = return address (at rbp + 8)
@@ -48,25 +34,15 @@ backtrace_loop:
     testq %r14, %r14
     jz backtrace_done        # Exit if no return address
     
-    # Again, another check to see if register is equal to or above 0x10000
-    cmpq $0x1000, %r14
-    jb backtrace_done
-   
     # Call dladdr(return_addr, &dl_info)
     movq %r14, %rdi          # first arg: return address
     leaq -64(%rbp), %rsi     # second arg: address of dl_info on stack
     call dladdr
-   
-    # Check if dladdr succeeded
-    testq %rax, %rax
-    jz print_unknown
-   
+    
     # get symbol name 
     movq -48(%rbp), %r15     # r15 = dli_sname (symbol name at offset 16 from dl_info)
-    testq %r15, %r15
-    jnz get_file_name
-    leaq unknown_sym(%rip), %r15
-   
+    
+    
 get_file_name:
     # get filename 
     movq -64(%rbp), %rbx     # rbx = dli_fname (file name at offset 0 from dl info)
@@ -74,26 +50,22 @@ get_file_name:
     jnz print_entry
     leaq unknown_file(%rip), %rbx
     jmp print_entry
-   
-print_unknown:
-    # dladdr failed, use unknown symbols
-    leaq unknown_sym(%rip), %r15
-    leaq unknown_file(%rip), %rbx
+    
 
 print_entry:
     # Call printf with format: "%3ld: [%lx] %s () %s\n"
     leaq backtrace_format_str(%rip), %rdi   # format string
-    movq %r12, %rsi                        # depth
-    movq %r14, %rdx                        # return address
-    movq %r15, %rcx                       # symbol name
-    movq %rbx, %r8                        # file name
-    xorq %rax, %rax                        # no vector registers used
+    movq %r12, %rsi                         # depth
+    movq %r14, %rdx                         # return address
+    movq %r15, %rcx                         # symbol name
+    movq %rbx, %r8                          # file name
+    xorq %rax, %rax                         # no vector registers used
     call printf
-   
+    
     # Move to next frame w/o printing
     movq (%r13), %r13        # r13 = next frame pointer
     incq %r12                # increment depth
-   
+    
     # continue the looping
     jmp backtrace_loop
 
@@ -101,14 +73,6 @@ backtrace_done:
     # cleans up stack
     addq $64, %rsp
     
-    # restore callee-saved registers
-    popq %r15
-    popq %r14
-    popq %r13
-    popq %r12
-    popq %rbx
-    
-    # Function epilogue
-    movq %rbp, %rsp
-    popq %rbp
+    # restores rbp and returns
+    leave
     ret
--- debug_skeleton_code/src/asm/dump_registers.s
+++ debug_skeleton_code/src/asm/dump_registers.s
@@ -3,66 +3,48 @@
 .type dump_registers, @function
 dump_registers:
 
-movq $5, %rax
-movq $7, %rbx
-movq $3, %rcx
-movq $-4, %rdx
+    push %r15
+    push %r14
+    push %r13
+    push %r12
+    push %r11
+    push %r10
+    push %r9
+    push %r8
 
-movq $4, %r10
-movq $2, %r8
-movq $3, %r9
-movq $11, %rsi
-movq $-14, %r14
-movq $15, %r15
-movq $24, %rdi
-
-# push %rsp
-#push %rbp and not use it later
-push %r15
-push %r14
-push %r13
-push %r12
-push %r11
-push %r10
-push %r9
-push %r8
-
-# Current RSP + 8 bytes (for each register pushed) + 8 bytes (return address)
-movq %rsp, %r8
-addq $(8*8 + 8), %r8
-push %r8            # Push original RSP
+    # Current RSP + 8 bytes (for each register pushed) + 8 bytes (return address)
+    movq %rsp, %r8
+    addq $(8*8 + 8), %r8
+    push %r8            # Push the original RSP
 
 
-push %rbp
-push %rdi
-push %rsi
-push %rdx
-push %rcx
-push %rbx
-push %rax
+    push %rbp
+    push %rdi
+    push %rsi
+    push %rdx
+    push %rcx
+    push %rbx
+    push %rax
 
+    # this puts the stack pointer as the first and only argument for _debug_dump_registers
+    movq %rsp, %rdi
+    call _debug_dump_registers
 
-    
-#subq $(16*8), %rsp
-movq %rsp, %rdi
-call _debug_dump_registers
-#addq $(16*8), %rsp
+    pop %rax
+    pop %rbx
+    pop %rcx
+    pop %rdx
+    pop %rsi
+    pop %rdi
+    pop %rbp
+    pop %r8         # takes off the copy of the original rbp
+    pop %r8         # takes off r8
+    pop %r9
+    pop %r10
+    pop %r11
+    pop %r12
+    pop %r13
+    pop %r14
+    pop %r15
 
-pop %rax
-pop %rbx
-pop %rcx
-pop %rdx
-pop %rsi
-pop %rdi
-pop %rbp
-pop %r8         # restore original rsp saved on stack
-pop %r8         # restore r8 pushed earlier
-pop %r9
-pop %r10
-pop %r11
-pop %r12
-pop %r13
-pop %r14
-pop %r15
-
-ret
+    ret
